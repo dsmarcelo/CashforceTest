13010642b329e802077c60bb6d66fac91f7ffd1f
dsmarcelo <celoygm@gmail.com>
2023-01-02 18:49:55 -0300

    feat: arquivo server.ts para iniciaro servidor

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, async () => {
+  console.log(`Servidor em: http://localhost:${port}`);
+});
