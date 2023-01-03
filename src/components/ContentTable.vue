35055fb7afb5513c2c6aa7ef01c413b62561059a
dsmarcelo <celoygm@gmail.com>
2023-01-03 11:57:59 -0300

    feat: Pagina de Notas Fiscais com tabela

---
+++ src/components/ContentTable.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import ContentRow from "./ContentRow.vue";
+
+defineProps({
+  data: Object,
+});
+</script>
+
+<template>
+  <div class="table-container">
+    <table class="table">
+      <thead>
+        <tr class="top-row">
+          <th>Nota fiscal</th>
+          <th>Sacado</th>
+          <th>Cedente</th>
+          <th>Emissão</th>
+          <th>Valor</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ContentRow v-for="item in data" :order="item" :key="item.id" />
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style>
+.table-container {
+  overflow-x: auto;
+  max-width: 1920px;
+}
+
+.table {
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 16px;
+}
+
+th {
+  padding-right: 12px;
+}
+
+table th:first-child,
+tr:first-child {
+  padding-left: 30px;
+}
+
+.top-row {
+  text-align: left;
+  color: var(--n-mid-1);
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 12px;
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <div class="page-header">
+    <div class="container">
+      <img class="icon-menu-proposta" src="/icon_menu_proposta.png" />
+      <h3 class="page-header-title">{{ title }}</h3>
+    </div>
+    <p class="page-header-description">
+      Visualize as notas fiscais que você tem.
+    </p>
+  </div>
+</template>
+
+<style>
+.page-header {
+  height: 53px;
+  max-width: 80%;
+  margin-bottom: 19px;
+}
+
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 9px;
+}
+
+.page-header-title {
+  color: var(--p-darkblue);
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+}
+
+.icon-menu-proposta {
+  width: 24px;
+}
+
+.page-header-description {
+  font-weight: 400;
+  font-size: 14px;
+  margin-top: 5px;
+  color: var(--n-dark-4);
+}
+</style>
